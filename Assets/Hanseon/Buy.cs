03955ae0afbad7dbbb01afb5e93b267b81b45038
RAHAI0 <112753290+RAHAI0@users.noreply.github.com>
2022-11-12 23:35:55 +0900

    [ADD] 인트로 스타트 엔드 음량조절

---
+++ Assets/Hanseon/Buy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buy : MonoBehaviour
+{
+    private Transform find;
+
+    public void CanBuy(int money)
+    {
+        if (money >= 10000)
+        {
+            find = GameObject.Find("ClothCanvas").transform.Find("Success");
+            find.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Hanseon/ChildSearch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChildSearch: MonoBehaviour
+{
+    Transform[] Scene2;
+    private float Time = 0f;
+    void Start()
+    {
+        foreach (Transform Child in this.transform)
+        {
+            Child.gameObject.SetActive(true);
+            StartCoroutine("timeCoroutine");
+        }
+
+    }
+
+    IEnumerator testCoroutine()
+    {
+        while (true)
+        {
+            Debug.Log("Do Something");
+            yield return new WaitForSeconds(5f);
+        }
+    }
+}
