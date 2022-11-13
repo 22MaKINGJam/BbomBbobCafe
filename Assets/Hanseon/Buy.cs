f177084ab2eea3b999b6a59ab0666e1a6fbe9ae3
RAHAI0 <112753290+RAHAI0@users.noreply.github.com>
2022-11-13 16:09:42 +0900

    [add] fadeout, graphic

--- Assets/Hanseon/Buy.cs
+++ Assets/Hanseon/Buy.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class Buy : MonoBehaviour
 {
     private Transform find;
+    public GameObject happy;
+    public GameObject gameui;
+    public AudioSource SuccessSound;
 
     public void CanBuy(int money)
     {
         if (money >= 10000)
         {
             find = GameObject.Find("ClothCanvas").transform.Find("Success");
+
             find.gameObject.SetActive(true);
+            happy.gameObject.SetActive(true);
+            gameui.gameObject.SetActive(false);
+            SuccessSound.Play();
         }
     }
 }
--- Assets/Hanseon/Change.cs
+++ Assets/Hanseon/Change.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Change : MonoBehaviour
 {
+    public Image image;
+
     public void GameExit()
     {
+        StartCoroutine(FadeCorountine());
         Application.Quit();
     }
-
     public void SceneMain()
     {
         SceneManager.LoadScene("Main");
@@ -17,5 +20,16 @@ public class Change : MonoBehaviour
     public void SceneStart()
     {
         SceneManager.LoadScene("Start");
+    }
+    IEnumerator FadeCorountine()
+    {
+        float fadeCount = 0;
+        while (fadeCount < 1.0f)
+        {
+            fadeCount+=1.0f;
+            yield return new WaitForSeconds(0.01f);
+            image.color = new Color(0, 0, 0, fadeCount);
+
+        }
     }
 }
